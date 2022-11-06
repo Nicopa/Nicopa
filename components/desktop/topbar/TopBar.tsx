fd873f8327c416dd25590039d47128924b5868e8
Nicolas Amarante <nicopa_rs@hotmail.com>
2022-11-06 17:30:03 -0300

    fix: clock rendering warning

--- components/desktop/topbar/TopBar.tsx
+++ components/desktop/topbar/TopBar.tsx
@@ -3,12 +3,10 @@ import styles from "./TopBar.module.scss";
 function Clock() {
 	const [time, setTime] = useState<Date>(new Date());
 	useEffect(() => {
-		return () => {
-			setTimeout(() => setTime(new Date()), 3500);
-		};
+		setTimeout(() => setTime(new Date()), 3500);
 	}, []);
 	return (
-		<span>
+		<span suppressHydrationWarning>
 			{time.getHours().toString().padStart(2, "0")}:
 			{time.getMinutes().toString().padStart(2, "0")}
 		</span>
