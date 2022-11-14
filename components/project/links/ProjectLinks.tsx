86445e52ade0c06440eabafa449d762c8afd22a4
Nicolas Amarante <nicopa_rs@hotmail.com>
2022-11-14 17:13:58 -0300

    update: project links new option live app

--- components/project/links/ProjectLinks.tsx
+++ components/project/links/ProjectLinks.tsx
@@ -1,15 +1,27 @@
 import { BsGithub } from "react-icons/bs";
+import { MdOutlineOpenInNew } from "react-icons/md";
 import styles from "./ProjectLinks.module.scss";
 
 type ProjectLinksProps = {
 	github?: string;
+	app?: string;
 };
-export default function ProjectLinks({ github }: ProjectLinksProps) {
+export default function ProjectLinks({ github, app }: ProjectLinksProps) {
 	return (
 		<div className={styles.root}>
 			{github && (
-				<a href={github} target="_blank" rel="noreferrer">
+				<a
+					href={github}
+					target="_blank"
+					rel="noreferrer"
+					title="GitHub project repository"
+				>
 					<BsGithub size={24} />
+				</a>
+			)}
+			{app && (
+				<a href={app} target="_blank" rel="noreferrer" title="Live app">
+					<MdOutlineOpenInNew size={24} />
 				</a>
 			)}
 		</div>
