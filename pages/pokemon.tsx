e5cd1ddeea0ef528e793ac8839b53211ce12116c
Nicopa <nicopa_rs@hotmail.com>
2024-06-19 16:53:22 -0300

    fix

--- pages/pokemon.tsx
+++ pages/pokemon.tsx
@@ -1,7 +1,7 @@
 export default function pokemonPage() {
     return (
         <div><iframe
-            src="/pokemon-memory-game/index.html"
+            src="./pokemon-memory-game/index.html"
             style={{ width: '100%', height: '100vh', border: 'none' }}
             title="Angular App"></iframe>
         </div>
